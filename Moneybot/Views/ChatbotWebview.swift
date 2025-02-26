54909906b54d6b139d5bb348cb702ad4b92c1a94
KahlilGarmon <19629497-KahlilGarmon@users.noreply.replit.com>
2025-02-26 09:56:26 +0000

    Replace Zapier integration with native OpenAI chatbot.

--- Moneybot/Views/ChatbotWebview.swift
+++ Moneybot/Views/ChatbotWebview.swift
@@ -1,35 +1,18 @@
 import SwiftUI
-import WebKit
 
-struct ChatbotView: UIViewRepresentable {
-    func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        let htmlString = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <script async type="module" src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js"></script>
-        </head>
-        <body>
-            <zapier-interfaces-chatbot-embed 
-                is-popup="true" 
-                chatbot-id="cm08ttupf0005yvzh163ldfcu" 
-                height="600px" 
-                width="400px">
-            </zapier-interfaces-chatbot-embed>
-        </body>
-        </html>
-        """
-        webView.loadHTMLString(htmlString, baseURL: nil)
-        return webView
-    }
+struct ChatbotView: View {
+    @StateObject private var moneybotService = MoneybotService.shared
     
-    func updateUIView(_ uiView: WKWebView, context: Context) {}
+    var body: some View {
+        // We'll simply use our existing MoneybotChatView which already has OpenAI integration
+        MoneybotChatView()
+    }
 }
+
 //
 //  ChatbotWebview.swift
 //  Moneybot
 //
 //  Created by Kahlil Garmon on 2/13/25.
+//  Updated to use native OpenAI integration
 //
-
